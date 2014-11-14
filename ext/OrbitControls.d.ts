104a34535ec4c4e25d8c092039acb56d96fea4f4
yutopp <yutopp@gmail.com>
2014-11-14 21:31:59 +0900

    fix interface

--- ext/OrbitControls.d.ts
+++ ext/OrbitControls.d.ts
@@ -2,6 +2,8 @@ declare module THREE {
     // incomplete...
     export class OrbitControls extends THREE.Object3D {
         constructor(camera: THREE.Camera, dom: HTMLCanvasElement);
+        update(): void;
+        reset(): void;
 
         damping: number;
     }
